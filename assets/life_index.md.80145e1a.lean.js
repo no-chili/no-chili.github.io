ad5f61887268e8279a4cfaf9731d3b40df4db1c3
no-chili <18875854140@163.com>
2022-10-11 17:56:31 +0800

    blog

---
+++ assets/life_index.md.80145e1a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.15c50983.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"life/index.md","lastUpdated":1665481237000}'),n={name:"life/index.md"};function r(o,s,c,d,i,p){return a(),t("div")}const l=e(n,[["render",r]]);export{f as __pageData,l as default};
